d81f74ed9d6d47529eea81dca1ac8aba13c4fc3b
Christian Gill <gillchristiang@gmail.com>
2017-05-13 19:32:59 -0300

    Update install.sh

--- bash/install.sh
+++ bash/install.sh
@@ -23,11 +23,6 @@ sudo apt-get install -y indicator-multiload ;
 # bison - curl - mercurial - make - binutils - gcc - build-essential
 sudo apt-get install -y bison curl mercurial make binutils gcc build-essential ;
 
-# golang
-sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable ;
-sudo apt-get update ;
-sudo apt-get -y install golang ;
-
 # spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 ;
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list ;
@@ -39,29 +34,7 @@ sudo apt-get install -y spotify-client ;
 ########################
 
 # node version manager
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | zsh ;
+curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash ;
 
 # yarn pkg
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - ;
-echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list ;
-
-sudo apt-get -y update && sudo apt-get install -y yarn ;
-
-# oh-my-zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh ;
-chsh -s `which zsh`;
-sudo shutdown -r 0 ;
-
-###########################
-# setup env - langs - etc #
-###########################
-
-# golang's $GOPATH
-
-mkdir ~/dev ;
-mkdir ~/dev/go ;
-mkdir ~/dev/go/bin ~/dev/go/src ;
-
-echo "# golang's $GOPATH"  >> ~/.zshrc ;
-echo 'export GOPATH="$HOME/dev/go"' >> ~/.zshrc ;
-echo 'export PATH="$PATH:$GOPATH/bin"' >> ~/.zshrc 
+curl -o- -L https://yarnpkg.com/install.sh | bash ;
